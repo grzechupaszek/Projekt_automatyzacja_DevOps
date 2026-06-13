0d8e8470f3e09b5f911a14bab7f77ef41beb19ee
Grzegorz Paszek <gpaszek@student.agh.edu.pl>
2026-06-13 09:56:04 +0200

    infra: dodaj tagi do resource group

--- infra/main.tf
+++ infra/main.tf
@@ -21,6 +21,12 @@ provider "azurerm" {
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
+
+  tags = {
+    project = "devops-2026-lab05"
+    author  = "Grzegorz Paszek"
+    album   = "422374"
+  }
 }
 
 resource "azurerm_container_registry" "main" {
